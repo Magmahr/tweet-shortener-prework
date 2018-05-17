049b9bf6bd5287629bb7dd98587d2956642be2fd
Matt Maher <mattmaher0@gmail.com>
2018-05-17 16:21:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,3 +35,10 @@ def bulk_tweet_shortener(array)
     puts "#{value}"
   end  
 end  
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else  
+    tweet
+end
